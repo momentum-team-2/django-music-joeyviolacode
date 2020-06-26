a02eeae4680784b4a5caaa1f91cd00241bff09d4
joeyviolacode <joeyviolacode@gmail.com>
2020-06-26 17:36:10 -0400

    More model migrations to assist in creating Artist objects.

--- albums/models.py
+++ albums/models.py
@@ -6,9 +6,17 @@ class Artist(models.Model):
 
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name}"
+
 class Album(models.Model):
 
     title = models.CharField(max_length=255)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="albums")
+    artist_string =  models.CharField(max_length=255)
     release_year = models.IntegerField()
     favorite = models.BooleanField(default=False)
+
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="albums")
+
+    def __str__(self):
+        return f"{self.title} - {self.artist}"
