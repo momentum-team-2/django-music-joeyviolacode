d2f25dd80cc7634f21ce366a20b4c34a2346c59d
joeyviolacode <joeyviolacode@gmail.com>
2020-06-26 12:31:24 -0400

    More files created and blocked out.

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+# from django.core.exceptions import ObjectDoesNotExist
+from .models import Album, Artist
+# from .forms import AlbumForm, ArtistForm
 
 # Create your views here.
+def list_albums(request):
+    pass
+
+def add_album(request):
+    pass
+
+def show_album(request, pk):
+    pass
+
+def edit_album(request, pk):
+    pass
+
+def delete_album(request, pk):
+    pass
