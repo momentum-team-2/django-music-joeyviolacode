0c56f7369275b659c3c5cb68c34b5f3206cbab9c
joeyviolacode <joeyviolacode@gmail.com>
2020-06-25 20:32:58 -0400

    Model and form alterations.

---
+++ albums/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Album, Artist
+
+class ArtistForm(forms.ModelForm):
+    class Meta:
+        model = Artist
+        fields = [
+            'name',
+        ]
+
+class AlbumForm(forms.ModelForm):
+    class Meta:
+        model = Album
+        fields = [
+            "title",
+            "release_year",
+        ]
--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+
+    title = models.CharField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="albums")
+    release_year = models.IntegerField()
+
+class Artist(models.Model):
+
+    name = models.CharField()
